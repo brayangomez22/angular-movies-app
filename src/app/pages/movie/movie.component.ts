0940bc811ba23481903ed4c21a307168e61d34cc
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-08-30 00:32:28 -0500

    Detail screen layout

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MoviesService } from '../../services/movies.service';
+import { MovieResponse } from '../../interfaces/movie-response';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-movie',
@@ -8,16 +10,23 @@ import { MoviesService } from '../../services/movies.service';
   styleUrls: ['./movie.component.scss'],
 })
 export class MovieComponent implements OnInit {
+  public movie: MovieResponse | undefined;
+
   constructor(
     private activatedRoute: ActivatedRoute,
-    private movieService: MoviesService
+    private movieService: MoviesService,
+    private location: Location
   ) {}
 
   ngOnInit(): void {
     const id = this.activatedRoute.snapshot.params.id;
 
     this.movieService.getMovieDetail(id).subscribe((movie) => {
-      console.log(movie);
+      this.movie = movie;
     });
+  }
+
+  onPrev() {
+    this.location.back();
   }
 }
