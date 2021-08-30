ce14795c1a3693cace855d41fbbd3d70396921f1
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-08-29 23:00:16 -0500

    InfiniteScroll de películas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { MoviesService } from 'src/app/services/movies.service';
 import { Movie } from '../../interfaces/billboard-response';
 
@@ -9,12 +9,28 @@ import { Movie } from '../../interfaces/billboard-response';
 })
 export class HomeComponent implements OnInit {
   public movies: Movie[] = [];
+  public moviesSlideshow: Movie[] = [];
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const pos =
+      (document.documentElement.scrollTop || document.body.scrollTop) + 1300;
+    const max =
+      document.documentElement.scrollHeight || document.body.scrollHeight;
+
+    if (pos > max) {
+      this.moviesService.getBillboard().subscribe((resp) => {
+        this.movies.push(...resp.results);
+      });
+    }
+  }
 
   constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
     this.moviesService.getBillboard().subscribe((resp) => {
       this.movies = resp.results;
+      this.moviesSlideshow = resp.results;
     });
   }
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -2,18 +2,35 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { map } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 import { BillboardResponse } from '../interfaces/billboard-response';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesService {
+  private baseUrl: string = 'https://api.themoviedb.org/3';
+  private billboardPage = 1;
+
   constructor(private http: HttpClient) {}
 
+  get params() {
+    return {
+      api_key: '920c2b99da9df62debac1e029e56f84b',
+      language: 'en-US',
+      page: this.billboardPage.toString(),
+    };
+  }
+
   getBillboard(): Observable<BillboardResponse> {
-    return this.http.get<BillboardResponse>(
-      'https://api.themoviedb.org/3/movie/now_playing?api_key=920c2b99da9df62debac1e029e56f84b&language=en-US&page=1'
-    );
+    return this.http
+      .get<BillboardResponse>(`${this.baseUrl}/movie/now_playing`, {
+        params: this.params,
+      })
+      .pipe(
+        tap(() => {
+          this.billboardPage += 1;
+        })
+      );
   }
 }
