72e0c9142cfde611eea53d21df36f290d10fd3d2
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-08-30 00:46:37 -0500

    Movie Actors and Error Handling

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MoviesService } from '../../services/movies.service';
 import { MovieResponse } from '../../interfaces/movie-response';
 import { Location } from '@angular/common';
+import { Cast } from '../../interfaces/credits-response';
 
 @Component({
   selector: 'app-movie',
@@ -11,18 +12,28 @@ import { Location } from '@angular/common';
 })
 export class MovieComponent implements OnInit {
   public movie: MovieResponse | undefined;
+  public cast: Cast[] | undefined;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private movieService: MoviesService,
-    private location: Location
+    private location: Location,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     const id = this.activatedRoute.snapshot.params.id;
 
     this.movieService.getMovieDetail(id).subscribe((movie) => {
+      if (!movie) {
+        this.router.navigateByUrl('/home');
+        return;
+      }
       this.movie = movie;
+    });
+
+    this.movieService.getCast(id).subscribe((cast) => {
+      this.cast = cast;
     });
   }
 
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -2,9 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
-import { tap, map } from 'rxjs/operators';
+import { tap, map, catchError } from 'rxjs/operators';
 import { BillboardResponse, Movie } from '../interfaces/billboard-response';
 import { MovieResponse } from '../interfaces/movie-response';
+import { CreditsResponse } from '../interfaces/credits-response';
 
 @Injectable({
   providedIn: 'root',
@@ -59,8 +60,21 @@ export class MoviesService {
   }
 
   getMovieDetail(id: string) {
-    return this.http.get<MovieResponse>(`${this.baseUrl}/movie/${id}`, {
-      params: this.params,
-    });
+    return this.http
+      .get<MovieResponse>(`${this.baseUrl}/movie/${id}`, {
+        params: this.params,
+      })
+      .pipe(catchError((err) => of(null)));
+  }
+
+  getCast(id: string) {
+    return this.http
+      .get<CreditsResponse>(`${this.baseUrl}/movie/${id}/credits`, {
+        params: this.params,
+      })
+      .pipe(
+        map((resp) => resp.cast),
+        catchError((err) => of([]))
+      );
   }
 }
